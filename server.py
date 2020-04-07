cb4e1bb23c636db88a39c0f70f19b91a20b1744c
Pierre Snell <pierre.snell.1@ulaval.ca>
2020-04-07 16:12:09 -0400

    feat : add asserts

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ import pdb
 
 import torch
 from flask import Flask, jsonify, request
-from spacy.lang.en import English
-from spacy.tokenizer import Tokenizer
+# from spacy.lang.en import English
+# from spacy.tokenizer import Tokenizer
 from torch import nn
 from transformers import (BertConfig, BertModel, BertTokenizer,
                           BertTokenizerFast, DistilBertConfig, DistilBertModel,
@@ -12,8 +12,8 @@ from transformers import (BertConfig, BertModel, BertTokenizer,
 
 adaptive_pool = nn.AdaptiveAvgPool1d(100)
 
-nlp = English()
-tokenizer = nlp.Defaults.create_tokenizer(nlp)
+# nlp = English()
+# tokenizer = nlp.Defaults.create_tokenizer(nlp)
 
 bc = BertConfig(output_hidden_states=True)
 # db = BertModel.from_pretrained("bert-base-multilingual-cased")
@@ -53,19 +53,19 @@ def tokenize():
 @app.route('/vectorize',  methods=['POST'])
 def vectorize():
     tokens = request.json["tokens"]
-    print(len(tokens))
+    # print("\n\n\n", tokens, "\n\n\n")
     embeddings = []
     with torch.no_grad():
         for tok in tokens:
             encoded = bert_tok.encode(tok, return_tensors="pt")
             out_tensor, hidden = bert(encoded)
 
-            out_tensor = adaptive_pool(out_tensor)
-            hidden = adaptive_pool(hidden.unsqueeze(0))
+            # out_tensor = adaptive_pool(out_tensor)
+            # hidden = adaptive_pool(hidden.unsqueeze(0))
 
-            out_tensor = torch.sum(out_tensor, axis=1).squeeze()
+            # out_tensor = torch.sum(out_tensor, axis=1).squeeze()
             hidden = hidden.squeeze()
-
+            assert len(hidden.size()) == 1
             embeddings.append(hidden.detach().cpu().data.numpy().tolist())
 
     # input_tensor = bert_tok.batch_encode_plus(tokens, pad_to_max_length=True,
@@ -75,14 +75,15 @@ def vectorize():
     # outputs = adaptive_pool(outputs[0])
     # outputs = outputs[0]
     # embeddings = outputs.squeeze().cpu().data.numpy().tolist()
-
+    assert len(embeddings) == len(tokens)
+    for e in embeddings:
+        assert len(e) == 768
     return jsonify({"vectors": embeddings})
 
 
 @app.route('/vectorize_utterances',  methods=['POST'])
 def vectorize_utterances():
     utterances = request.json["utterances"]
-    print(len(utterances))
     with torch.no_grad():
         input_tensor = bert_tok.batch_encode_plus(utterances,
                                                   pad_to_max_length=True,
@@ -93,8 +94,9 @@ def vectorize_utterances():
 
         # outputs = adaptive_pool(outputs[0])
         # embedding = torch.sum(outputs, axis=1)
-        embedding = adaptive_pool(hidden.unsqueeze(0))
-        embeddings = embedding.detach().squeeze().cpu().data.numpy().tolist()
+        # embedding = adaptive_pool(hidden.unsqueeze(0)).squeeze()
+        embedding = hidden
+        embeddings = embedding.detach().cpu().data.numpy().tolist()
     return jsonify({"vectors": embeddings})
 
 
@@ -103,8 +105,7 @@ def info():
     infos = {
         "version": "1",
         "ready": True,
-        # "dimentions": 768,
-        "dimentions": 100,
+        "dimentions": 768,
         "domain": "bp",
         "readOnly": True,
         "languages": [
@@ -116,3 +117,19 @@ def info():
 
 if (__name__ == '__main__'):
     app.run(debug=True)
+
+
+# Botpress tokenizer mon_vectorize
+
+# Botpress tokenizer mon_sentence
+
+# size 100 / 300 / 786
+
+
+# Test 1 vectorize 768 bert puis botpress
+# Test 2 vectorize 300 bert puis botpress
+# Test 3 vectorize 100 bert puis botpress
+
+# Test 4 tokenize botpress puis vecotrize sentence bert 768
+# Test 4 tokenize botpress puis vecotrize sentence bert 300
+# Test 4 tokenize botpress puis vecotrize sentence bert 100
